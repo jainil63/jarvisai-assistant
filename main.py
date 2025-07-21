648754633a08f878b386eba69900df8a1146fc82
jainil <jainilgondaliya63@gmail.com>
2025-07-21 22:09:59 +0530

    convert to voice

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voices', voices[-1].id)
+engine.runAndWait()
+
+def speak(audio):
+    engine.say(audio)
+    print(audio)
+    engine.runAndWait()
+
 def main():
-    print("Hello i am jarvis")
+    speak("Hello i am jarvis")
 
 
 if __name__ == "__main__":
